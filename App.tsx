beeaf646a35bbf94a74ba868306434ff6fb16b5a
acculief <yajousaki@gmail.com>
2019-12-29 22:33:30 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, ViewStyle, TextStyle} from 'react-native';
+
+interface Props {}
+
+interface State {}
+
+export default class App extends Component<Props, State> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native</Text>
+        <Text style={styles.instructions}></Text>
+        <Text style={styles.instructions}>Cmd+D or shake for dev menu</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  } as ViewStyle,
+
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  } as TextStyle,
+
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  } as TextStyle,
+});
